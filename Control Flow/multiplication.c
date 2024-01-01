37083f2899fc610b178ecb0fae454b5165beb507
Debasish Bhola <devop69@gmail.com>
2024-01-01 11:46:22 +0530

    Completed printTable.c

--- Control Flow/multiplication.c
+++ Control Flow/multiplication.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
-void main(){
+int printTable(int num){
+    int mul;
+    for (int i = 0; i <= 10; i++)
+    {
+        mul = num*i;
+
+        printf("%d * %d = %d",num,i,mul);
+        printf("\n");
+    }
     
+}
+void main(){
+    int num = 5;
+    printTable(5);
 }
