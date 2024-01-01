ceaf9cab9d61d134197d518a080e36ed446e930a
Debasish Bhola <devop69@gmail.com>
2024-01-01 11:21:45 +0530

    Completed Factorial

--- Control Flow/factorial.c
+++ Control Flow/factorial.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 int fact(int n){
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n*fact(n-1);
     
 }
 void main(){
-    
+    int n;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("Factorial of %d is %d",n,fact(n));
 }
